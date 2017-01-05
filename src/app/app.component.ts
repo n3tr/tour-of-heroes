da956e60f45cea552dbe655de6e0145badf00df8
Jirat K <saakyz@gmail.com>
2017-01-05 13:49:42 +0700

    Chapter 2 SelectHero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+]
 
 @Component({
   selector: 'app-root',
@@ -7,9 +20,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes'
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  heroes = HEROES
+  selectedHero: Hero
+
+  onSelect(hero) {
+    this.selectedHero = hero
   }
 }
 
